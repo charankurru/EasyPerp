a38310239177d2c6a8b68d48ab3e6130567b84f9
charankurru <61787496+charankurru@users.noreply.github.com>
2020-04-23 07:45:39 +0530

    connecting to mongoAtlas2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 var monk = require('monk');
 //var db = monk('URI  || localhost:27017/easyprep');
 var db = monk(
-  'mongodb+srv://charan:bharathi@cluster0-2hbtz.mongodb.net/test?retryWrites=true&w=majority'
+  process.env.MONGODB_URI ||
+    'mongodb+srv://charan:bharathi@cluster0-2hbtz.mongodb.net/test?retryWrites=true&w=majority'
 );
 var perdet = db.get('personalDetails');
 var acadet = db.get('AcademicDetails');
