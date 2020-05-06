910c0cbf4a3d343dff0aa8b006ce8e73b8bc3e80
charankurru <61787496+charankurru@users.noreply.github.com>
2020-05-06 22:24:04 +0530

    features addes

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -5,7 +5,11 @@ app.controller('myCtrl', function (
   posdata,
   podata,
   pdata,
-  editdata
+  editdata,
+  getdata,
+  getdat,
+  getda,
+  getd
 ) {
   // $scope.user.firstName = 'John';
   // $scope.user.lastName = 'Doe';
@@ -21,74 +25,77 @@ app.controller('myCtrl', function (
 
   $scope.user = { firstName: 'John', lastname: 'Doe' };
   $scope.send = function (val) {
-    $scope.Astore.push(val);
+    //$scope.Astore.push(val);
     postdata.postingdata(val);
     //insertdata.postingdata(val);
     $scope.user = {}; // calling the post function
   };
 
   $scope.go = function (val) {
-    $scope.Bstore.push(val);
+    //$scope.Bstore.push(val);
     posdata.postingdata(val);
     $scope.obj = {}; // calling the post function
   };
 
   $scope.went = function (val) {
-    $scope.Cstore.push(val);
+    //$scope.Cstore.push(val);
     podata.postingdata(val);
     $scope.det = {}; // calling the post function
   };
 
   $scope.gone = function (val) {
-    $scope.Dstore.push(val);
+    //$scope.Dstore.push(val);
     pdata.postingdata(val);
     $scope.dat = {}; // calling the post function
   };
 
-  // editing the data
+  // editing the data______________________________________________________________________________________
   $scope.update = function (val) {
     editdata.editingdata(val);
   };
-});
-//--_______________________________________________________________________________________________________________________________________--
 
-app.service('postdata', function ($http) {
-  return {
-    postingdata: function (value) {
-      $http({
-        method: 'post',
-        url: '/postdata',
-        data: value,
-      }).then(
-        function (success) {
-          console.log(success);
-        },
-        function (error) {
-          console.log(error);
-        }
-      );
-    },
+  //getting data from database_ and storing them in a array_____________________________________________________________________________
+  $scope.get = function (val) {
+    console.log(val);
+    getdata.gettingdata(val).then(function (data) {
+      if (data) {
+        $scope.Astore = data;
+        console.log($scope.store);
+      } else {
+        $scope.Astore = [];
+      }
+    });
+
+    getdat.gettingdata(val).then(function (data) {
+      if (data) {
+        $scope.Bstore = data;
+        console.log($scope.store);
+      } else {
+        $scope.Bstore = [];
+      }
+    });
+
+    getda.gettingdata(val).then(function (data) {
+      if (data) {
+        $scope.Cstore = data;
+        console.log($scope.store);
+      } else {
+        $scope.Cstore = [];
+      }
+    });
+
+    getd.gettingdata(val).then(function (data) {
+      if (data) {
+        $scope.Dstore = data;
+        console.log($scope.store);
+      } else {
+        $scope.Dstore = [];
+      }
+    });
   };
 });
 
-// app.service('insertdata', function($http) {
-//   return {
-//     postingdata: function(value) {
-//       $http({
-//         method: 'post',
-//         url: '/getda',
-//         data: value
-//       }).then(
-//         function(success) {
-//           console.log(success);
-//         },
-//         function(error) {
-//           console.log(error);
-//         }
-//       );
-//     }
-//   };
-// });
+// service is used to making the response _______________________________________________________________________________________________________________________________________--
 
 app.service('postdata', function ($http) {
   return {
@@ -181,6 +188,72 @@ app.service('editdata', function ($http) {
           console.log(error);
           alert(error.data);
         };
+    },
+  };
+});
+
+//______________________________________________________________________________________________________________________________________
+// factory is used to get an response from the backend
+app.factory('getdata', function ($http) {
+  return {
+    gettingdata: function (value) {
+      dattas = $http({
+        method: 'get',
+        url: '/getdata',
+        data: value,
+      }).then(function (response) {
+        console.log(response.data);
+        return response.data;
+      });
+      return dattas;
+    },
+  };
+});
+
+app.factory('getdat', function ($http) {
+  return {
+    gettingdata: function (value) {
+      dattas = $http({
+        method: 'get',
+        url: '/getdat',
+        data: value,
+      }).then(function (response) {
+        console.log(response.data);
+        return response.data;
+      });
+      return dattas;
+    },
+  };
+});
+
+app.factory('getda', function ($http) {
+  return {
+    gettingdata: function (value) {
+      dattas = $http({
+        method: 'get',
+        url: '/getda',
+        data: value,
+      }).then(function (response) {
+        console.log(response.data);
+        return response.data;
+      });
+      return dattas;
+    },
+  };
+});
+
+app.factory('getd', function ($http) {
+  return {
+    gettingdata: function (value) {
+      dattas = $http({
+        method: 'get',
+        url: '/getd',
+        data: value,
+      }).then(function (response) {
+        console.log(response.data);
+        return response.data;
+      });
+      return dattas;
     },
   };
 });
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 
 var router = express.Router();
 var monk = require('monk');
-// var db = monk('localhost:27017/easyprep');
-var db = monk(
-  process.env.MONGODB_URL ||
-    'mongodb+srv://charan:bharathi@cluster0-2hbtz.mongodb.net/easyPrep?retryWrites=true&w=majority'
-);
+var db = monk('localhost:27017/easyprep');
+// var db = monk(
+//   process.env.MONGODB_URL ||
+//     'mongodb+srv://charan:bharathi@cluster0-2hbtz.mongodb.net/easyPrep?retryWrites=true&w=majority'
+// );
 var perdet = db.get('personalDetails');
 var acadet = db.get('AcademicDetails');
 var perskill = db.get('personlSkill');
@@ -16,10 +16,6 @@ var session = db.get('login&sign');
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('home', { title: 'Express' });
-});
-
-router.get('/forms', function (req, res, next) {
-  res.render('index');
 });
 //________________________________________________________________________________________________________________________
 /*posting the data form all forms into respective collections*/
@@ -75,7 +71,7 @@ router.get('/second', function (req, res, next) {
   res.render('second');
 });
 //_______________________________________________________________________________________________________________________
-// finding the data
+// finding the data previosly used for rendering the data into temp arrays
 
 router.post('/getAdata', function (req, res, next) {
   //console.log(req.body);
@@ -116,19 +112,20 @@ router.post('/edit', function (req, res) {
 });
 //____________________________________________________________________________________________________________________
 
-// sign up
+// sign up__________________________________________________________________________________________________________
 router.post('/signup', function (req, res, nxt) {
   session.insert(req.body, function (err, docs) {
     if (err) {
       console.log('err');
     } else {
+      alert('successfully registered');
       console.log(docs);
       res.redirect('/');
     }
   });
 });
 
-//login
+//login____________________________________________________________________________________________________________________
 router.post('/login', function (req, res) {
   var uname = req.body.Username;
   var pws = req.body.password;
@@ -139,13 +136,14 @@ router.post('/login', function (req, res) {
       delete docs.password;
       req.session.usable = docs; // we have assigned docs to an object called usable
       console.log(req.session.usable);
-      res.redirect('/forms');
+      res.render('index', { profile: req.session.usable });
     } else {
       console.log(err);
     }
   });
 });
 
+// proceeding the data into frontend___________________________________________________________________________________________________
 router.get('/proceed', function (req, res) {
   if (req.session && req.session.usable) {
     res.locals.usable = req.session.usable;
@@ -157,9 +155,129 @@ router.get('/proceed', function (req, res) {
         console.log(err);
       } else {
         console.log(docs);
-        res.render('output', { project: docs });
+        personal_details = docs;
+        //res.render('output', { project: docs });
+      }
+    });
+
+    acadet.find({ lastname: req.session.usable.Username }, function (
+      err,
+      docs
+    ) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(docs);
+        academic_details = docs;
+        res.render('output', {
+          data1: personal_details,
+          data2: academic_details,
+        });
+      }
+    });
+
+    perskill.find({ lastname: req.session.usable.Username }, function (
+      err,
+      docs
+    ) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(docs);
+        perskill_details = docs;
+      }
+    });
+
+    techdet.find({ lastname: req.session.usable.Username }, function (
+      err,
+      docs
+    ) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(docs);
+        techical_details = docs;
+        res.render('output', {
+          data1: personal_details,
+          data2: academic_details,
+          data3: perskill_details,
+          data4: techical_details,
+        });
       }
     });
   }
 });
+
+// logout___________________________________________________________________________________________________________________________
+
+router.get('/logout', function (req, res) {
+  req.session.reset();
+  res.redirect('/');
+});
+
+// getting the users only data________________________________________________________________________________________________________
+router.get('/getdata', function (req, res, nxt) {
+  if (req.session && req.session.usable) {
+    res.locals.usable = req.session.usable;
+    perdet.find({ lastname: req.session.usable.Username }, function (
+      err,
+      docs
+    ) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(docs);
+      }
+    });
+  }
+});
+
+router.get('/getdat', function (req, res, nxt) {
+  if (req.session && req.session.usable) {
+    res.locals.usable = req.session.usable;
+    acadet.find({ lastname: req.session.usable.Username }, function (
+      err,
+      docs
+    ) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(docs);
+      }
+    });
+  }
+});
+
+router.get('/getda', function (req, res, nxt) {
+  if (req.session && req.session.usable) {
+    res.locals.usable = req.session.usable;
+    techdet.find({ lastname: req.session.usable.Username }, function (
+      err,
+      docs
+    ) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(docs);
+      }
+    });
+  }
+});
+
+router.get('/getd', function (req, res, nxt) {
+  if (req.session && req.session.usable) {
+    res.locals.usable = req.session.usable;
+    perskill.find({ lastname: req.session.usable.Username }, function (
+      err,
+      docs
+    ) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(docs);
+      }
+    });
+  }
+});
+
 module.exports = router;
